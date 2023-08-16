5ea1276442c98810d4fdd446cd08df0d589694a6
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2023-08-16 15:12:43 -0300

    Adicionando função de excluir e alterar status
    Adicionando função de excluir afazer pelo botão e de alterar status do afazer através do click com js.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
-const botao = document.querySelector("#btn");
+const botoes = document.querySelectorAll("#btn");
+const stats = document.querySelectorAll("#stats");
+const c = "✔ COMPLETADO"
+const a = "EM ANDAMENTO"
 
-botao.addEventListener("click", () => {
-    pai = botao.parentElement
+botoes.forEach((e) => e.addEventListener("click", () => {
+    pai = e.parentElement
     pai.remove()
-})
+}))
+
+stats.forEach((e) => e.addEventListener("click", () => {
+    const lista = e.classList
+    if(lista == "main_container_block_status2"){
+        lista.remove("main_container_block_status2")
+        lista.add("main_container_block_status1")
+        e.innerHTML = c
+    } else {
+        lista.remove("main_container_block_status1")
+        lista.add("main_container_block_status2")
+        e.innerHTML = a
+    }
+}))
