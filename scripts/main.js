f2fa8ecd0ad08ffa0ca26c07d11e130a529f83ec
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2023-08-18 08:49:02 -0300

    Adicionando todas as funcionalidades
    Adicionando as funcionalidades de criação e exclusão dos afazeres.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,11 @@ const botoes = document.querySelectorAll("#btn");
 const stats = document.querySelectorAll("#stats");
 const btnAdd = document.querySelector("#btnAdd");
 const caixaTexto = document.querySelector("#input");
-const btnComp = document.querySelectorAll("#stats_choice1")[1];
-const btnAnd = document.querySelectorAll("#stats_choice2")[2];
+const btnComp = document.querySelector("#stats_choice1");
+const btnAnd = document.querySelector("#stats_choice2");
 const c = "✔ COMPLETADO";
 const a = "EM ANDAMENTO";
+const container = document.querySelector("#container");
 
 botoes.forEach((e) => e.addEventListener("click", () => {
     pai = e.parentElement
@@ -31,9 +32,22 @@ const estruturaCodigo = (pai, tag, classe) => {
     pai.appendChild(comando)
 }
 
+const criaBtn = () => {
+    const button = document.createElement("input");
+    button.type = "button"
+    button.value = "-"
+    button.id = "btn"
+    button.className = "main_container_block_btn"
+    return button
+}
+
+document.querySelector("h1").addEventListener("click", () => {
+    console.log("Deu?")
+    criaBtn()
+})
+
 btnAdd.addEventListener("click", () => {
     const texto = caixaTexto.value;
-    const container = document.querySelector("#container");
     estruturaCodigo(container, "section", "main_container_block")
     const sec = document.querySelectorAll(".main_container_block")
     const section = sec[(sec.length)-1]
@@ -42,14 +56,24 @@ btnAdd.addEventListener("click", () => {
     const section2 = sec2[(sec.length)-1]
     estruturaCodigo(section2, "p", "main_container_block_text")
     estruturaCodigo(section2, "p", "main_container_block_status2")
-    const button = document.createElement("input")
-    button.type = "button"
-    button.value = "-"
-    button.className = "main_container_block_btn"
-    button.id = "btn"
-    estruturaCodigo(section, "input", "main_container_block_btn")
     const boxes = document.querySelectorAll(".main_container_block_text")
     boxes[(boxes.length)-1].innerHTML = texto
+    if(btnComp.checked === true){
+        const p = document.createElement("p")
+        p.id = "stats"
+        p.className = "main_container_block_status1"
+        section2.appendChild(p)
+        p.innerHTML = c
+        console.log(btnComp.checked)
+    } else {
+        const p = document.createElement("p")
+        p.id = "stats"
+        p.className = "main_container_block_status2"
+        section2.appendChild(p)
+        p.innerHTML = a
+        console.log(btnComp.checked)
+    }
+    section.appendChild(criaBtn())
 })
 
 
